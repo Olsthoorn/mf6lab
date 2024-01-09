43bbfc130d9e104278ae1f4ff0f9457af2647f87
Theo Olsthoorn <tolsthoorn@google.com>
2024-01-09 17:40:44 +0100

    Adding concept structure for Density project with cases Henry and Blankenberge. Conceprs only.

---
+++ Projects/Density/cases/Blankenberge/mf_run.py
@@ -0,0 +1,30 @@
+# Run Modflow etc for the current Case
+import os
+import logging
+
+import mf_adapt
+from src import mf_setup
+
+logging.info("Running from {}".format(os.getcwd()))
+
+gr = mf_adapt.gr
+
+fp_packages, model_dict, use_models, use_packages = mf_setup.mf_setup()
+
+sim = fp_packages.get('Simsim')
+
+# Write simulation
+sim.write_simulation(silent=False)
+
+# Run simulation
+success, buff = sim.run_simulation(silent=False)
+
+print('Running success = {}'.format(success))
+if not success:
+    print(buff)
+    logging.critical("Buffer printed because MODFLOW did not terminate normally.")
+    raise Exception('MODFLOW did not terminate normally.')
+
+# TODO Indicate the seepage zone.
+
+print("Done")
