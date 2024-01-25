1af14f2be613d514d0d52c0f75a219f4984887fe
Theo Olsthoorn <tolsthoorn@google.com>
2024-01-25 08:27:50 +0100

    Project wells introduced.

---
+++ Projects/Wells/cases/TheisHantush/mf_adapt.py
@@ -0,0 +1,112 @@
+# Wells, Theis and Hantush
+"""
+An axially symmetric model is setup to compute the drawdown by wells, both Theis and Hantush type as well as any
+multi-layer variants, such that all cases like delayed yield  and well bore storage can be delt with by setting the parameters appropriately.
+
+The idea is to show the stream lines and the water table in the cross section,
+"""
+
+# TO 20240124
+
+import os
+import numpy as np
+from src import mf6tools
+import settings
+
+INACTIVE =[-1, 0]
+
+dirs = settings.dirs
+sim_name = settings.sim_name
+section_name = settings.section_name
+lay = settings.lay
+pr = settings.props
+gr = settings.gr
+params_wbk = settings.params_wbk # Parameters workbook
+
+# === tdis ===== period data:
+start_date_time = pr['start_date_time'] # Must be a string.
+
+perDF = mf6tools.get_periodata_from_excel(params_wbk, sheet_name='PER')
+period_data = [tuple(sp) for sp in perDF[['PERLEN', 'NSTP', 'TSMULT']].values]
+
+# Stress period start times
+sp_start_times = np.datetime64(start_date_time) + np.cumsum(
+                  np.hstack((0., np.array([p[0] for p in period_data])[:-1]))
+                  ) * np.timedelta64(1, 'D')
+
+Simtdis = {'perioddata': period_data,
+           'nper': len(period_data),
+           'start_date_time': start_date_time,
+           'time_units': settings.TIME_UNITS,
+           }
+
+# === DIS ===== Grid, structured
+
+IDOMAIN = gr.const(1, dtype=int)
+IDOMAIN[gr.DZ < pr['minDz']] = -1
+
+Gwfdis = {'gr': gr,
+          'idomain': IDOMAIN,
+          'length_units': settings.LENGTH_UNITS}
+
+# ==== Gwfsto ===== Storage, transient
+
+Gwfsto = {'sy': gr.const(lay['Sy'].values),
+          'ss': gr.const(lay['Ss'].values),
+          'iconvert': gr.const(lay['ICELLTYPE'].values),
+          }
+
+# === Gwfnpf ===== Horizontal and vertical conductivity
+
+Gwfnpf = {  'k':   gr.const(lay['k'].values),
+            'k33': gr.const(lay['k33'].values),
+            'icelltype': gr.const(lay['ICELLTYPE'].values),
+            }
+
+#  === Gwfic ===== Initial condictions (head)
+
+strthd = gr.const(pr['strthd'])
+
+Gwfic = {'strt': strthd}
+
+# === Gwfwel ===== wells
+
+Iglob_well = gr.NOD[:, 0, 0][lay['Screened'] > 0]
+stress_period_data = [(lrc, pr['Q']) for lrc in gr.lrc_from_iglob(Iglob_well)]
+Gwfwel = {'stress_period_data': stress_period_data,
+          'auto_flow_reduce': 0.1,
+          }
+
+# === Gwfdrn ===== drains
+hDr = gr.Z[0, 0] - pr['drain_depth']
+drn_xyz = np.vstack((gr.xm, np.zeros(gr.nx), hDr)).T
+Iz = gr.lrc_from_xyz(drn_xyz)['ic'][:, 0]
+gr.top_active_cells(IDOMAIN, Iz)
+
+Cdr = gr.Area[0] / pr['cDrainage']
+      
+DRN = [((iz, 0, i), h_, C_) for i, (iz, h_, C_) in enumerate(zip(Iz, hDr, Cdr))]
+
+Gwfdrn = {'stress_period_data': {0: DRN}}
+
+# === Gwfchd ===== Fixed head period data
+
+# chd instead of drains
+CHD = [((iz, 0, i), pr['strthd']) for i, iz in enumerate(Iz)]
+Gwfchd = {'stress_period_data': CHD}
+
+# === Gwfrch ===== recharge
+
+RCH = [((iz, 0, i), pr['rch']) for i, iz in enumerate(Iz)]
+
+Gwfrch = {'stress_period_data': {0: RCH}}
+
+# === Gwfoc ====== Output control for flow model
+
+Gwfoc = {'head_filerecord':   os.path.join(dirs.SIM, "{}Gwf.hds".format(sim_name)),
+         'budget_filerecord': os.path.join(dirs.SIM, "{}Gwf.cbc".format(sim_name)),
+         'saverecord': [("HEAD", "FREQUENCY", pr['oc_frequency']),
+                        ("BUDGET", "FREQUENCY", pr['oc_frequency'])],
+}
+
+print('Done mf_adapt')
---
+++ Projects/Wells/cases/TheisHantush/settings.py
@@ -0,0 +1,89 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle, Path
+
+from src.mf6tools import  Dirs
+from fdm.mfgrid import Grid
+from etc import newfig
+
+HOME = '/Users/Theo/GRWMODELS/python/mf6lab/Projects/Wells/'
+assert os.path.isdir(HOME), "Can't find the directory {}".format(HOME)
+
+LENGTH_UNITS = 'meters'
+TIME_UNITS = 'days'
+
+dirs = Dirs(HOME)
+
+section_name = 'Theis Hantush Delayed yield'
+sim_name = 'TheisHantush'
+dirs = dirs.add_case(sim_name)
+xmlfile = os.path.join(dirs.data, sim_name + '.xml')
+
+os.chdir(dirs.case)
+
+params_wbk = os.path.join(dirs.case, sim_name + '.xlsx')
+assert os.path.isfile(params_wbk), "Params_wbk not found: {}".format(params_wbk)
+
+lay = pd.read_excel(params_wbk, sheet_name='LAY', header=0, index_col=0)
+
+props = { 'start_date_time': '2024-01-24',
+         'oc_frequency': 1,
+         'L': 10000.0, # [m] width of model
+         'rw': 0.1,
+         'minDz': 0.25, # m min layer thickness
+          'drain_depth': 1e-5, # m
+          'cDrainage': 100., # d
+          'rch': 0.001, # m/d
+          'strthd': 0.0, # m initial head
+          'dz_pinched_out': 0.01, # [m] thickness of pinched out layers"
+          'Q' : -125.0, # m3/d'
+          'ztop': 1.0,
+}
+
+props['R'] = props['L']
+r = np.hstack((0.0, np.logspace(np.log10(props['rw']), np.log10(props['R']), 81), props['R'] + 0.01))
+z = props['ztop'] - np.cumsum([0, *lay['D']], axis=0)
+         
+gr = Grid(r, [], z, axial=True, min_dz = props['dz_pinched_out'])
+
+
+if __name__ == '__main__':
+    
+    # Show the results
+    title = section_name
+    ax = newfig(title, "Lijnafstand (m)", "mTAW")
+
+    layer_patches = gr.layer_patches_x(row=0) # Get layer patches
+    
+    ax.plot([0, props['L']], [0, 0], 'darkblue', label='test watertafel')
+    
+    for p, clr, code, name in zip(layer_patches, lay['Color'], lay['Code'], lay['Name']):
+        p.set_fc(clr)
+        p.set_ec('k')
+        p.set_alpha(1.0)
+        p.set_lw(0.25)
+        p.set_label(code + ' ' + name)
+        ax.add_patch(p)
+        
+    # well screen patches:
+    for ilay, screened in lay['Screened'].items():
+        if screened:
+            zt, zb = gr.z[ilay], gr.z[ilay + 1]            
+            p = Rectangle([0.0, zb], props['rw'], zt - zb)
+            p.set_fc('black')
+            p.set_ec('black')
+            p.set_alpha(0.8)
+            p.set_lw(0.25)
+            p.set_label('screen layer {}'.format(ilay))
+            ax.add_patch(p)
+            
+    ax.set_xlim(0.0, 5)
+
+    ax.legend()
+    
+    print("Done")
+    
+    plt.show()
+
